807c4933b650276432b8838b06f0133324ef927b
crypi <cry.pierre@outlook.fr>
2019-05-09 15:34:06 +0200

    Quelques modifs 16

--- java_files/Main.java
+++ java_files/Main.java
@@ -5,17 +5,11 @@ import java.util.*;
 public class Main {
     
     public static void main(String[] args) {
-
-        LinkedList<Integer> ll1, ll2;
-        LinkedList<Instr> example_code ;
-		LinkedList<Couple<String,LinkedList<Instr>>> fds;
         Config cfg;
-
         cfg = new Config(new NullV(),Gen.code,LLE.empty(),LLE.empty());
         cfg.exec();
         cfg.getValue().printValue();
         System.out.println();
-        
     }
 
 }
