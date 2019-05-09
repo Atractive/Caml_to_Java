2fed4e52607a67029f39594227a7f79c4de08a60
crypi <cry.pierre@outlook.fr>
2019-05-09 09:16:35 +0200

    Mise en place du fichier config

--- java_files/Config.java
+++ java_files/Config.java
@@ -2,55 +2,71 @@ import java.util.*;
 
 class Config extends Object {
 	
-    Value v;
-    LinkedList<Instr> c;
-    LinkedList<StackElem> s;
+    Value vv;
+    LinkedList<Instr> cc;
+    LinkedList<StackElem> ss;
+	Map<String,LinkedList<Instr>> fds;
 	
 	/* Constructors */
-    public Config (Value vl, LinkedList<Instr> cd, LinkedList<StackElem> se) {
-        v = vl;
-        c = cd;
-        s = se;
+    public Config (Value v, LinkedList<Instr> c, LinkedList<StackElem> s, Map<String,LinkedList<Instr>> f) {
+        vv = v;
+        cc = c;
+        ss = s;
+		fds = f;
     }
+	
+	/* Set */
+	
+	void set_value(Value v) {
+        this.vv = v;
+    }
+	
+    void set_code(LinkedList<Instr> c) {
+        this.cc = c;
+    }
+	
+    void get_stack(LinkedList<StackElem> s) {
+        this.ss = s;
+    }
+	
+	void set_fds(Map<String,LinkedList<Instr>> f) {
+		this.fds = f;
+	}
+	
+	/* Get */
 
     Value get_value() {
-        return v;
+        return this.vv;
     }
 	
     LinkedList<Instr> get_code() {
-        return c;
+        return this.cc;
     }
 	
     LinkedList<StackElem> get_stack() {
-        return s;
+        return this.ss;
     }
+	
+	Map<String,LinkedList<Instr>> get_fds() {
+		return this.fds;
+	}
+	
+	/* exec */
 
-    void set_value(Value nv) {
-        v = nv;
-    }
-	
-    void set_code(LinkedList<Instr> nc) {
-        c = nc;
-    }
-	
-    void get_stack(LinkedList<StackElem> ns) {
-        s = ns;
-    }
-	
     // one-step execution 
     boolean exec_step() {
-        // to be implemented
-        return true;
+        if (c.isEmpty()) {return false;} 
+		else {return true;}
     }
 
     // run to completion
     void exec() {
-        // to be implemented
+        while (exec_step()) {}
     }
 
     // run for n steps
     void step(int n) {
-        // to be implemented
+        System.out.print(n);
     }
     
 }
