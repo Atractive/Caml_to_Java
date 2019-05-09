623251b3f5149a0d7acffd5e7c2ab1da2dc542e6
crypi <cry.pierre@outlook.fr>
2019-05-09 15:44:01 +0200

    Quelques modifs 18

--- java_files/Main.java
+++ java_files/Main.java
@@ -6,7 +6,7 @@ public class Main {
     
     public static void main(String[] args) {
         Config cfg;
-        cfg = new Config(new NullV(),Gen.code,new LinkedList<Instr>(),new LinkedList<Instr>());
+        cfg = new Config(new NullV(),Gen.code,new LinkedList<StackElem>(),new LinkedList<Couple<String,LinkedList<Instr>>>());
         cfg.exec();
         cfg.getValue().printValue();
         System.out.println();
